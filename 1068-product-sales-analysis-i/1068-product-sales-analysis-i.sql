3efcf41f07ad63b0e21137a3a60d1b6eadd05368
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-10-23 18:18:03 +0530

    Update 1068-product-sales-analysis-i.sql

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,1 +1,9 @@
+solution 1:
+select Product.product_name, Sales.year, Sales.price
+from Sales
+left join Product
+on Sales.product_id = Product.product_id
 
+solution 2:
+select p.product_name,s.year,s.price 
+from Sales s join Product p on s.product_id=p.product_id;
