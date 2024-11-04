44c2d0ee8929e51ca0c4ebac9434f74f7b572e9f
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-11-05 00:49:40 +0530

    Update 577-Employee-bonus.sql

--- 577-Employee-bonus/577-Employee-bonus.sql
+++ 577-Employee-bonus/577-Employee-bonus.sql
@@ -1,1 +1,21 @@
+Solution 1:
 
+select x.name , y.bonus 
+from Employee x 
+left join Bonus y 
+on x.empId=y.empId 
+having y.bonus<1000 or y.bonus is null;
+
+Solution 2:
+
+with first as (select e.empId, 
+    e.name, 
+    b.bonus, 
+    isnull(b.bonus, 0) as bonus_edit
+from Employee e
+left join Bonus b
+on e.empId = b.empId)
+
+select name, bonus
+from first
+where bonus_edit < 1000
